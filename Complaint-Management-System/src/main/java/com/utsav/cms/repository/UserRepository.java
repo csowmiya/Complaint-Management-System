e3f1b3f3d4cc3b0163118c007bf5e85cdf1a17a5
Sowmiya <137800581+csowmiya@users.noreply.github.com>
2025-07-05 20:49:09 +0530

    Update UserRepository.java

--- Complaint-Management-System/src/main/java/com/utsav/cms/repository/UserRepository.java
+++ Complaint-Management-System/src/main/java/com/utsav/cms/repository/UserRepository.java
@@ -1,11 +1,11 @@
-package com.utsav.cms.repository;
+package com.cms.repository;
 
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import com.utsav.cms.model_bean.User;
+import com.cms.model_bean.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
 	@Query("from User where fname = :name or "
